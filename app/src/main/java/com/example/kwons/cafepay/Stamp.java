8982367df3d54288af0441d578d9c1c3c3e1c08f
kwonsye <kwonsye@naver.com>
2018-08-01 22:00:53 +0900

    coupons/{userId}/count 구현

--- app/src/main/java/com/example/kwons/cafepay/Stamp.java
+++ app/src/main/java/com/example/kwons/cafepay/Stamp.java
@@ -1,5 +1,6 @@
 package com.example.kwons.cafepay;
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Stamp {
@@ -7,15 +8,19 @@ public class Stamp {
 
     //스탬프개수
     @SerializedName("angelinusCouponCount")
-    public int angelinusStampCount;
+    @Expose public int angelinusStampCount;
+
     @SerializedName("ediyaCouponCount")
-    public int ediyaStampCount;
+    @Expose public int ediyaStampCount;
+
     @SerializedName("hollysCouponCount")
-    public int hollysStampCount;
+    @Expose public int hollysStampCount;
+
     @SerializedName("starbucksCouponCount")
-    public int starbucksStampCount;
+    @Expose public int starbucksStampCount;
+
     @SerializedName("tomntomsCouponCount")
-    public int tomntomsStampCount;
+    @Expose public int tomntomsStampCount;
 
 
 
